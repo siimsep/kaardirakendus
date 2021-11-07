6da921c0556c548a0e0a7c42100f1680cd58672c
siimsep <siimsep@tlu.ee>
2021-11-07 16:12:08 +0200

    7.11 services and controllers done.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response, Application } from "express";
-
-//import db from "./db";
 import usersController from "./components/users/controller";
-//import responseCodes from "./components/general/responseCodes";
+import jobController from "./components/jobs/controller";
 import { port } from "./components/general/settings";
 
+/*
+ * *********************** Initializing Express.js API ******************
+ */
 const app: Application = express();
 app.use(express.json());
 
@@ -16,112 +17,15 @@ app.get("/users/:id", usersController.getUserById);
 app.delete("/users/:id", usersController.removeUser);
 app.post("/users", usersController.createUser);
 app.patch("/users/:id", usersController.updateUser);
+/*
+ * *********************** Jobs ******************
+ */
+app.get("/jobs", jobController.getAllJobs);
+app.get("/jobs/:id", jobController.getJobById);
+app.delete("/jobs/:id", jobController.removeJob);
+app.post("/jobs", jobController.createJob);
+app.patch("/jobs/:id", jobController.updateJob);
 
-/* // SHOWING THE JOBLIST
-app.get("/jobs", (req: Request, res: Response) => {
-  const { jobList } = db;
-  return res.status(responseCodes.ok).json({
-    jobList,
-  });
-});
-
-app.get("/jobs/:id", (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id, 10);
-  if (!id) {
-    return res.status(responseCodes.badRequest).json({
-      error: "No valid id provided",
-    });
-  }
-  const job = db.jobList.find((element) => element.id === id);
-  if (!job) {
-    return res.status(responseCodes.badRequest).json({
-      error: `No job found with id: ${id}`,
-    });
-  }
-  return res.status(responseCodes.ok).json({
-    job,
-  });
-});
-/////////////////////////////////////////////////////
-
-// ADDING JOBS
-app.post("/jobs", (req: Request, res: Response) => {
-  const { lat, lng, note } = req.body;
-  if (!lat) {
-    return res.status(responseCodes.badRequest).json({
-      error: "Error in coordinates: no latitude",
-    });
-  }
-  if (!lng) {
-    return res.status(responseCodes.badRequest).json({
-      error: "Error in coordinates: no longitude",
-    });
-  }
-  if (!note) {
-    return res.status(responseCodes.badRequest).json({
-      error: "Error, please add note",
-    });
-  }
-
-  const id = db.jobList.length + 1;
-  db.jobList.push({
-    id,
-    lat,
-    lng,
-    note,
-    completion: false,
-  });
-  return res.status(responseCodes.created).json({
-    id,
-    message: "Great success!",
-  });
-});
-/////////////////////////////////////////////////////
-
-// DELETING JOBS
-app.delete("/jobs/:id", (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id, 10);
-  if (!id) {
-    return res.status(responseCodes.badRequest).json({
-      error: "No valid id provided",
-    });
-  }
-  const index = db.jobList.findIndex((element) => element.id === id);
-  if (index < 0) {
-    return res.status(responseCodes.badRequest).json({
-      message: `Job not found with id: ${id}`,
-    });
-  }
-  db.jobList.splice(index, 1);
-  return res.status(responseCodes.noContent).send();
-});
-
-// CHANGING JOB STATUS
-app.patch("/jobs/:id", (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id, 10);
-  const { completion } = req.body;
-  if (!id) {
-    return res.status(responseCodes.badRequest).json({
-      error: "No valid id provided",
-    });
-  }
-  if (!completion) {
-    return res.status(responseCodes.badRequest).json({
-      error: "Nothing to update",
-    });
-  }
-  const index = db.jobList.findIndex((element) => element.id === id);
-  if (index < 0) {
-    return res.status(400).json({
-      error: `No job found with id: ${id}`,
-    });
-  }
-  if (completion) {
-    db.jobList[index].completion = true;
-  }
-
-  return res.status(responseCodes.noContent).send();
-}); */
 /////////////////////////////////////////////////////
 
 app.listen(port, () => {
